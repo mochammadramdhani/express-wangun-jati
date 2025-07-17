4218b64c81507574934e0681d19afdd050f22410
mochammadramdhani <mochammadramdhani22@gmail.com>
2025-07-17 21:16:06 +0700

    ubah frontend url

--- config/main.config.js
+++ config/main.config.js
@@ -1,11 +1,11 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 const CONFIG = {
-    SUPABASE_URL: process.env.SUPABASE_URL,
-    SUPABASE_KEY: process.env.SUPABASE_KEY,
-    JWT_SECRET: process.env.JWT_SECRET,
-    FRONTEND_URL: "http://localhost:3000",
-}
+  SUPABASE_URL: process.env.SUPABASE_URL,
+  SUPABASE_KEY: process.env.SUPABASE_KEY,
+  JWT_SECRET: process.env.JWT_SECRET,
+  FRONTEND_URL: "https://wangun-jati.vercel.app",
+};
 
-export { CONFIG }
+export { CONFIG };
